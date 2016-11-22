e1c4e4ee3e3b9b64252b0e5e04d78e42b19c39b5
Panagiotis Zestanakis <panosz@gmail.com>
2016-11-22 18:26:11 +0200

    Project extracted from AUG equilibrium

---
+++ Interpolant1D.cpp
@@ -0,0 +1,21 @@
+/*
+ * Interpolant1D.cpp
+ *
+ *  Created on: Oct 4, 2016
+ *      Author: panosz
+ */
+
+#include "Interpolant1D.h"
+
+//namespace Panos_1DInterp {
+//
+//Interpolant1D::Interpolant1D() {
+//	// TODO Auto-generated constructor stub
+//
+//}
+//
+//Interpolant1D::~Interpolant1D() {
+//	// TODO Auto-generated destructor stub
+//}
+//
+//} /* namespace Panos_1DInterp */
