4cfb3cefb6339e7f202b5a3439a3ddbb8e0fa11a
Panagiotis Zestanakis <panosz@gmail.com>
2018-07-10 13:47:02 +0300

    updated gsl_wrappers.hpp to include more headers by default

--- src/GSL_wrappers/include/gsl_wrappers.hpp
+++ src/GSL_wrappers/include/gsl_wrappers.hpp
@@ -4,6 +4,11 @@
 
 #include "matrix.hpp"
 #include "Interpolant1D.hpp"
+#include "hermite_polynomial_interpolation.hpp"
+#include "linear_least_quares.hpp"
+#include "matrix.hpp"
+#include "minimization.hpp"
+
 
 #endif//GSL_WRAPPERS
 
